894ce795dae0a45540de8ad3abfd818eb49def3e
Roberto Maier <robertomaier02@gmail.com>
2023-09-07 22:10:50 -0300

    add novas rotas.

--- routes/apiV2Router.js
+++ routes/apiV2Router.js
@@ -1,38 +1,86 @@
-const express = require('express')
-const apiV2Router = express.Router()
-const knex = require('knex')(require('../knexfile.js')
-[process.env.NODE_ENV])
+const express = require('express');
+const apiV2Router = express.Router();
+const knex = require('knex')(require('../knexfile.js')[process.env.NODE_ENV]);
 
-// processar o corpo da requisição e colocar os dados em req.body
-apiV2Router.use (express.json())
+// Process the request body and populate req.body
+apiV2Router.use(express.json());
 
 //---------------------------------------------- API
+
+// obter todos os produtos
 apiV2Router.get('/produtos', (req, res) => {
     knex('produtos')
-    .then (produtos => {
-        res.status(200).json(produtos)
-    })
-    .catch(err => {
-        res.status(500).json({
-            message : "erro meu guri"
+        .then(produtos => {
+            res.status(200).json(produtos);
         })
-    })
-})
+        .catch(err => {
+            res.status(500).json({
+                message: 'Um erro ocorreu: ' + err.message,
+            });
+        });
+});
 
+// obter o produto especifico por   ID
 apiV2Router.get('/produtos/:id', (req, res) => {
-const id = parseInt (req.params.id)
-
+    const id = parseInt(req.params.id);
     knex('produtos')
-    .when()
+        .then(produtos => {
+            const idx = produtos.findIndex(p => p.id === id);
+            res.status(200).json(produtos[idx]);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Um erro ocorreu: ' + err.message,
+            });
+        });
+});
 
-})
-
+// criar um novo produto
 apiV2Router.post('/produtos', (req, res) => {
+    const { nome, preco } = req.body;
+    knex('produtos')
+        .insert({ nome, preco })
+        .then(result => {
+            res.status(201).json(result);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Um erro ocorreu: ' + err.message,
+            });
+        });
+});
 
-})
+// atualizar um produto por ID
+apiV2Router.put('/produtos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const { nome, preco } = req.body;
+    knex('produtos')
+        .where('id', id)
+        .update({ nome, preco })
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Um erro ocorreu: ' + err.message,
+            });
+        });
+});
 
-apiV2Router.put('/produtos', (req, res) => {})
+// deletar um produto por ID
+apiV2Router.delete('/produtos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    knex('produtos')
+        .where('id', id)
+        .del()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Um erro ocorreu: ' + err.message,
+            });
+        });
+});
 
-apiV2Router.delete('/produtos', (req, res) => {})
-
-module.exports = apiV2Router
+module.exports = apiV2Router;
